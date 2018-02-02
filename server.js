8ae98a6aa816c9d3b4d310d529e0a0ef3d6299ce
alexey <alexey.trofim.1@gmail.com>
2018-02-02 23:34:47 +0200

    123

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var server = http.createServer();
 // Настройка слушателя событий для запроса
 server.on('request', function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end(util.inspect(http,true,5,true));
+   res.end(util.inspect(http,true,4,true));
 })
 server.listen(3000);
 console.log('Server running at http://localhost:3000/');
